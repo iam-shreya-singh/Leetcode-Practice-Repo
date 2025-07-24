75f1f9b423624a43defb4e79307ff9f0e7e49fbb
Shreya Singh <shreyasinghin24@gmail.com>
2025-07-24 18:54:00 +0530

    Time: 4 ms (57.18%), Space: 14.8 MB (36.52%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) 
+    {
+        std::unordered_map<int, int> numToIndexMap; // value -> index
+
+        for (int i = 0; i < nums.size(); i++) {
+            int diff = target - nums[i];
+
+            if (numToIndexMap.find(diff) != numToIndexMap.end()) {
+                return {numToIndexMap[diff], i}; // return the index of diff and current index
+            }
+
+            numToIndexMap[nums[i]] = i; // store current number and its index
+        }
+
+        return {}; // in case no solution found
+    }
+};
